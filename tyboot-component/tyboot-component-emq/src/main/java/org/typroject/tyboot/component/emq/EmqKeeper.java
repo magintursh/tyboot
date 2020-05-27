b8e61f27cc2649fdd0f7c2f3700b15440c5609cb
子杨 <magintursh69@163.com>
2020-05-27 11:32:14 +0800

    自定义emqx链接中的option参数 maxInflight

--- tyboot-component/tyboot-component-emq/src/main/java/org/typroject/tyboot/component/emq/EmqKeeper.java
+++ tyboot-component/tyboot-component-emq/src/main/java/org/typroject/tyboot/component/emq/EmqKeeper.java
@@ -37,6 +37,7 @@ public class EmqKeeper {
         options.setCleanSession(emqProperties.getCleanSession());// 设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接
         options.setConnectionTimeout(0);// 设置超时时间 单位为秒
         options.setKeepAliveInterval(10);// 设置会话心跳时间 单位为秒 服务器会每隔1.5*20秒的时间向客户端发送个消息判断客户端是否在线，但这个方法并没有重连的机制
+        options.setMaxInflight(emqProperties.getMaxInflight());
         client.setCallback(new DefaultCallback());// 设置回调
         client.setTimeToWait(5000);
         connetToServer();
--- tyboot-component/tyboot-component-emq/src/main/java/org/typroject/tyboot/component/emq/EmqProperties.java
+++ tyboot-component/tyboot-component-emq/src/main/java/org/typroject/tyboot/component/emq/EmqProperties.java
@@ -16,6 +16,7 @@ public class EmqProperties {
     private String broker; //emq服务器地址
     private String clientId;//客户端id
     private Boolean cleanSession;// 设置是否清空session,这里如果设置为false表示服务器会保留客户端的连接记录，这里设置为true表示每次连接到服务器都以新的身份连接
+    private int maxInflight = 100;
 
 
     public Boolean getCleanSession() {
@@ -58,5 +59,13 @@ public class EmqProperties {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public int getMaxInflight() {
+        return maxInflight;
+    }
+
+    public void setMaxInflight(int maxInflight) {
+        this.maxInflight = maxInflight;
     }
 }
